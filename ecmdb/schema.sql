9a8e1e739c83b06846024f78a08c5973cb401ee4
Seth Troisi <sethtroisi@google.com>
2019-06-15 03:29:14 -0700

    Added initial server.py, schema.sql

--- ecmdb/schema.sql
+++ ecmdb/schema.sql
@@ -1,49 +1,53 @@
+/** DB for tracking distribution ecm effort */
 
-/** Main table distribution ecm effort */
+/* TODO: delete before 1.0.0 */
+DROP TABLE IF EXISTS ecm_curve;
+DROP TABLE IF EXISTS numbers;
+DROP TABLE IF EXISTS factors;
 
 CREATE TABLE IF NOT EXISTS ecm_curve (
-  PRIMARY_KEY(num_id, curve_id),
-
-  num_id INTEGER NOT NULL REFERENCES(numbers),
+  num_id INTEGER NOT NULL,
   curve_id INTEGER NOT NULL,
 
   B1 INTEGER NOT NULL,
   B2 INTEGER NOT NULL,
 
-  stage1_chkpnt text,
+  stage1_chkpnt TEXT,
 
   maxmem INTEGER NOT NULL,
   stage1_ms INTEGER NOT NULL,
   stage2_ms INTEGER NOT NULL,
 
   /* ecm = 1, pm1 = 2, pp1 = 3 */
-  method INTEGER NOT NULL CHECK(method >= 1 and method <= 3),
+  method INTEGER NOT NULL CHECK(method >= 1 AND method <= 3),
 
-  timestamp INTEGER NOT NULL
-
+  timestamp INTEGER NOT NULL,
 
   /* TODO: x, y, param, sigma */
   /* TODO: A, torsion, k, power, dickson */
   /* TODO: ecm-version */
-)
+
+  FOREIGN KEY (num_id) REFERENCES numbers(num_id),
+  PRIMARY KEY (num_id, curve_id)
+);
 
 CREATE TABLE IF NOT EXISTS numbers (
-  num_id INTEGER NOT NULL PRIMARY KEY
-  n text NOT NULL,
-  n_expr NOT NULL,
-
+  num_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  n      TEXT NOT NULL,
+  n_expr TEXT NOT NULL,
   /**
    * P = 1, PRP = 2, FF = 3,
    * CF = 4, C = 5
    */
-  status INTEGER NOT NULL CHECK(status >= 1 and status <= 5),
-)
+  status INTEGER NOT NULL CHECK(status >= 1 AND status <= 5)
+);
 
 CREATE TABLE IF NOT EXISTS factor (
-  PRIMARY KEY(num_id_c, num_id_f),
+  num_id_c INTEGER NOT NULL,
+  num_id_f INTEGER NOT NULL,
 
-  num_id_c INTEGER NOT NULL REFERENCES(numbers),
-  num_id_f INTEGER NOT NULL REFERENCES(numbers)
-
-)
+  FOREIGN KEY(num_id_c) REFERENCES numbers(num_id),
+  FOREIGN KEY(num_id_f) REFERENCES numbers(num_id),
+  PRIMARY KEY(num_id_c, num_id_f)
+);
 
--- ecmdb/server.py
+++ ecmdb/server.py
@@ -1,7 +1,9 @@
 import datetime
 import os
 import logging
+import re
 import time
+import gmpy2
 
 import sqlite3
 
@@ -13,21 +15,52 @@ class EcmServer:
 
     SCHEMA_FILE = "schema.sql"
 
-    def __init__(db_file="ecm-server.db"):
+    def __init__(self, db_file="./ecm-server.db"):
         self.db_file = db_file
         self.db = None
-        init_db()
+
+        self.init_db()
 
 
-    def init_db():
+    def init_db(self):
+        exists = os.path.isfile(self.db_file)
+
         self.db = sqlite3.connect(self.db_file)
         # Makes returns namedtuple like
-        #self.db.row_factory = sqlite3.Row
+        self.db.row_factory = sqlite3.Row
+
+        # Turn on foreign_key constraints
+        self.db.execute("PRAGMA foreign_keys = 1")
+
+        if not exists:
+            schema_path = os.path.join(os.path.dirname(__file__), EcmServer.SCHEMA_FILE)
+            logging.warn(f"Creating db({self.db_file}) from {schema_path}")
+            with open(schema_path) as schema_f:
+                schema = schema_f.read()
+                cur = self.db.cursor()
+                cur.executescript(schema)
+                cur.close()
+
+    def add_number(self, expr):
+        # https://stackoverflow.com/questions/2371436/evaluating-a-mathematical-expression-in-a-string
+
+        if re.match("[1-9][0-9]*", expr):
+            # TODO: verify not already in db
+
+            n = int(expr)
+        else:
+            # TODO: Implement
+            short = str(expr)[:100]
+            error = f"Not yet able to eval expr({short})"
+            logging.error(error)
+            raise NotImplementedError(error)
+
+        status = 2 if gmpy2.is_prime(n) else 5
 
         cur = self.db.cursor()
-
-        if not os.path.isfile(self.db_file):
-            logger.info(f"Creating db({self.db_file}) from {EcmServer.SCHEMA_FILE}")
-            with open(EcmServer.SCHEMA_FILE) as schema_f:
-                cur.executescript(schema_f.read())
+        cur.execute(
+            'INSERT INTO numbers VALUES (1,?,?,?)',
+            (n, expr, status))
+        cur.close()
+        self.db.commit()
 
