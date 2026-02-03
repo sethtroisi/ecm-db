102f410ac1bb610ac084f88dbb98a496dbf04746
Seth Troisi <sethtroisi@google.com>
2026-02-03 00:50:12 -0800

    QoL in delete_finished

--- tools/delete_finished.py
+++ tools/delete_finished.py
@@ -1,15 +1,28 @@
 """Removes lines from a partial file that have already been processed in a ecm_runner_X.log file."""
 
+import argparse
 import re
 import sys
 
 
+def get_argparser():
+    parser = argparse.ArgumentParser(description='delete finished lines from a resume file.')
+    parser.add_argument('-n', '--dry-run', action='store_true',
+                        help="Dry Run, don't save file")
+    parser.add_argument('resume_file', type=str,
+                        help='resume file, finished lines will be deleted from this file.')
+    parser.add_argument('log_file', type=str,
+                        help='log file to serach for finished results.')
+    return parser
+
+
 def read_log(fn):
+  INPUT_MATCH = re.compile(r"^Input number is (.*) \([0-9]+ digits\)$")
   finished = set()
   with open(fn) as f:
     for line in f:
       if line.startswith("Input number is"):
-        match = re.match("^Input number is (.*) \([0-9]+ digits\)$", line)
+        match = INPUT_MATCH.match(line)
         assert match
         N = match.group(1)
         finished.add(N)
@@ -27,32 +40,46 @@ def N_matches(line, finished):
   return False
 
 
-def remove_matched(resume_fn, result_fn, finished):
-  matched = 0
+def remove_matched(args, result_fn, finished):
+  saved = 0
+  filtered = 0
 
-  with open(resume_fn) as f, open(result_fn, "w") as copy_f:
+  should_save = not args.dry_run
+
+  with open(args.resume_file) as f:
+    if should_save:
+      print(f"Saving filtered results to {result_fn!r}")
+      copy_f =  open(result_fn, "w")
+
     for line in f:
       if N_matches(line, finished):
-        matched += 1
+        filtered += 1
         continue
-      copy_f.write(line)
 
-  print(f"Removed {matched}/{len(finished)} finished results")
+      saved += 1
+      if should_save:
+        copy_f.write(line)
+
+  if should_save:
+      copy_f.close()
+
+  n = saved + filtered
+  print(f"Removed {filtered}/{n} finished results, {saved}/{n} remaining lines")
 
 
-def main(resume_fn, log_fn):
-  finished = read_log(log_fn)
+def main(args):
+  finished = read_log(args.log_file)
   if not finished:
     print(f"No finished results in {log_fn!r}")
     exit(1)
 
-  remove_matched(resume_fn, resume_fn + ".filtered", finished)
+  new_name = args.resume_file + ".filtered"
+  remove_matched(args, new_name, finished)
 
 
 
 if __name__ == "__main__":
-  if len(sys.argv) != 3:
-    print("{sys.argv[0]} takes 2 args: resume_fn and log_fn")
-    exit(1)
+  parser = get_argparser()
+  args = parser.parse_args()
 
-  main(sys.argv[1], sys.argv[2])
+  main(args)
